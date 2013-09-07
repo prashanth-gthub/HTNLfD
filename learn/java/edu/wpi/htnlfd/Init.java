9ba6292c3ac4cf16c88b3ff6e20803963e2d9611
Anahita Mohseni Kabir <anahitam70@gmail.com>
2013-09-07 13:09:15 -0400

    Formatting Init.java

--- learn/java/edu/wpi/htnlfd/Init.java
+++ learn/java/edu/wpi/htnlfd/Init.java
@@ -7,21 +7,21 @@ import edu.wpi.disco.lang.*;
 
 public class Init {
 
-	public static void main(String[] args) {
-		System.out.println("YUHU");
-	}
+   public static void main (String[] args) {
+      System.out.println("YUHU");
+   }
 
-	public static void demonstration(Disco disco,String taskModelName) {
-	   System.out.println(taskModelName);
-	   Segment parent = disco.getStack().get(0);
-	   for (Iterator<Object> children = parent.children(); children.hasNext();) {
-	      Object child = children.next();
-	      if ( child instanceof Task ) {
+   public static void demonstration (Disco disco, String taskName) {
+      System.out.println(taskName);
+      Segment parent = disco.getStack().get(0);
+      for (Iterator<Object> children = parent.children(); children.hasNext();) {
+         Object child = children.next();
+         if ( child instanceof Task ) {
             Task task = (Task) child;
             if ( !(task instanceof Utterance) ) {
                System.out.println(task.getType());
             }
          }
-	   }
-	}
+      }
+   }
 }
