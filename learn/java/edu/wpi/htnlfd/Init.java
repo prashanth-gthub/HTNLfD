28319ec03827289193bddd71518fef9f29999df7
Anahita Mohseni Kabir <anahitam70@gmail.com>
2013-09-07 10:04:49 -0400

    Reformatting Init.java

--- learn/java/edu/wpi/htnlfd/Init.java
+++ learn/java/edu/wpi/htnlfd/Init.java
@@ -1,11 +1,8 @@
 package edu.wpi.htnlfd;
 
-import org.w3c.dom.Document;
-import java.io.PrintStream;
 import java.util.*;
 import edu.wpi.cetask.*;
 import edu.wpi.disco.*;
-import edu.wpi.disco.Agenda.Plugin;
 import edu.wpi.disco.lang.*;
 
 public class Init {
@@ -18,15 +15,13 @@ public class Init {
 	   System.out.println(taskModelName);
 	   Segment parent = disco.getStack().get(0);
 	   for (Iterator<Object> children = parent.children(); children.hasNext();) {
-         Object child = children.next();
-         if ( child instanceof Task ) {
+	      Object child = children.next();
+	      if ( child instanceof Task ) {
             Task task = (Task) child;
-            
             if ( !(task instanceof Utterance) ) {
                System.out.println(task.getType());
             }
          }
 	   }
-
 	}
 }
