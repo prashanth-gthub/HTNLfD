66b1641726a1d0e86ad5b9b28f0deb4acdd2ebff
Anahita Mohseni Kabir <anahitam70@gmail.com>
2013-12-02 23:24:37 -0500

    Revised

--- learn/java/edu/wpi/htnlfd/carMaintenance/Fluid.java
+++ learn/java/edu/wpi/htnlfd/carMaintenance/Fluid.java
@@ -2,14 +2,15 @@ package edu.wpi.htnlfd.carMaintenance;
 
 public class Fluid extends PhysObj {
    public Level level;
+
    public Fluid (String name, Location location, Level level) {
       super(name, location);
       this.level = level;
    }
 
-   
    public static final PhysObj Oil = new Fluid("Oil", null, null);
 
-   public static final PhysObj RadiatorFluid = new Fluid("RadiatorFluid", null, null);
+   public static final PhysObj RadiatorFluid = new Fluid("RadiatorFluid", null,
+         null);
 
 }
