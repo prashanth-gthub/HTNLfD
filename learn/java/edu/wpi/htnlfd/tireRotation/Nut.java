3d8dc20f80d02aff3f572a84f65f2bea29ccd3a8
Anahita Mohseni Kabir <anahitam70@gmail.com>
2014-01-20 23:27:55 -0500

    Tire Rotation

--- learn/java/edu/wpi/htnlfd/tireRotation/Nut.java
+++ learn/java/edu/wpi/htnlfd/tireRotation/Nut.java
@@ -7,4 +7,6 @@ public class Nut extends PhysObj {
    public Nut (String name, Location location) {
       super(name, location);
    }
+   
+   public static Nut Nut1 = new Nut("Nut1",null);
 }
---
+++ learn/java/edu/wpi/htnlfd/tireRotation/Stud.java
@@ -0,0 +1,12 @@
+package edu.wpi.htnlfd.tireRotation;
+
+import edu.wpi.htnlfd.domain.*;
+
+public class Stud extends PhysObj {
+
+   public Stud (String name, Location location) {
+      super(name, location);
+   }
+   
+   public static Stud Stud1 = new Stud("Stud1",null);
+}
