57f23ca65bc66362e088797719baa05fb0f8e0fa
Anahita Mohseni Kabir <anahitam70@gmail.com>
2014-01-20 19:46:40 -0500

    Nut, CarWorld added

---
+++ learn/java/edu/wpi/htnlfd/tireRotation/Nut.java
@@ -0,0 +1,10 @@
+package edu.wpi.htnlfd.tireRotation;
+
+import edu.wpi.htnlfd.domain.*;
+
+public class Nut extends PhysObj {
+
+   public Nut (String name, Location location) {
+      super(name, location);
+   }
+}
---
+++ learn/java/edu/wpi/htnlfd/world/CarWorld.java
@@ -0,0 +1,10 @@
+package edu.wpi.htnlfd.world;
+
+import edu.wpi.htnlfd.tireRotation.Nut;
+import java.util.*;
+
+public class CarWorld extends World{
+
+   public static final List<Nut> LOOSE_NUTS = new ArrayList<Nut>();
+
+}
