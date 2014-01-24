26c0be3b6f7ad5ea421bef502c407553f74250e5
Anahita <anahita@vivid.cs.wpi.edu>
2014-01-24 15:07:43 -0500

    Tire Rotation

--- learn/java/edu/wpi/htnlfd/tireRotation/Car.java
+++ learn/java/edu/wpi/htnlfd/tireRotation/Car.java
@@ -7,60 +7,28 @@ public class Car extends PhysObj {
 		super(name, location);
 	}
 
-	private Tire RFTire = new Tire("RFTire", Location.plus(this.location,
+	public Tire RFtire = new Tire("RFtire", Location.plus(this.location,
 			new Location(10, 10, 0)));
 
-	private Tire LFTire = new Tire("LFTire", Location.plus(this.location,
+	public Tire LFtire = new Tire("LFtire", Location.plus(this.location,
 			new Location(-10, -10, 0)));
 
-	private Tire RRTire = new Tire("RRTire", Location.plus(this.location,
+	public Tire RRtire = new Tire("RRtire", Location.plus(this.location,
 			new Location(10, -10, 0)));
 
-	private Tire LRTire = new Tire("LRTire", Location.plus(this.location,
+	public Tire LRtire = new Tire("LRtire", Location.plus(this.location,
 			new Location(-10, 10, 0)));
 
-	private Wheel RFWheel = new Wheel("RFWheel", Location.plus(this.location,
+	public Wheel RFwheel = new Wheel("RFwheel", Location.plus(this.location,
 			new Location(10, 10, 0)));
 
-	private Wheel LFWheel = new Wheel("LFWheel", Location.plus(this.location,
+	public Wheel LFwheel = new Wheel("LFwheel", Location.plus(this.location,
 			new Location(-10, -10, 0)));
 
-	private Wheel RRWheel = new Wheel("RRWheel", Location.plus(this.location,
+	public Wheel RRwheel = new Wheel("RRwheel", Location.plus(this.location,
 			new Location(10, -10, 0)));
 
-	private Wheel LRWheel = new Wheel("LRWheel", Location.plus(this.location,
+	public Wheel LRwheel = new Wheel("LRwheel", Location.plus(this.location,
 			new Location(-10, 10, 0)));
 
-	public Tire getRFTire() {
-		return RFTire;
 	}
-
-	public Tire getLFTire() {
-		return LFTire;
-	}
-
-	public Tire getRRTire() {
-		return RRTire;
-	}
-
-	public Tire getLRTire() {
-		return LRTire;
-	}
-
-	public Wheel getRFWheel() {
-		return RFWheel;
-	}
-
-	public Wheel getLFWheel() {
-		return LFWheel;
-	}
-
-	public Wheel getRRWheel() {
-		return RRWheel;
-	}
-
-	public Wheel getLRWheel() {
-		return LRWheel;
-	}
-
-}
--- learn/java/edu/wpi/htnlfd/tireRotation/Wheel.java
+++ learn/java/edu/wpi/htnlfd/tireRotation/Wheel.java
@@ -8,22 +8,10 @@ public class Wheel extends PhysObj {
 		super(name, location);
 	}
 
-	private Stud studA = new Stud(name + "_StudA", null);
+	public Stud studA = new Stud(name + "_StudA", null);
 
-	private Stud studB = new Stud(name + "_StudB", null);
+	public Stud studB = new Stud(name + "_StudB", null);
 
-	private Stud studC = new Stud(name + "_StudC", null);
-
-	public Stud getStudA() {
-		return studA;
-	}
-
-	public Stud getStudB() {
-		return studB;
-	}
-
-	public Stud getStudC() {
-		return studC;
-	}
+	public Stud studC = new Stud(name + "_StudC", null);
 
 }
